d95cbec1ce0824efc7a1b96e0ecc7115c7666434
Juan Pablo Fresia <39677236+JuanPF56@users.noreply.github.com>
2021-05-03 11:32:29 -0300

    Implementación de items a, b y c.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Trabajo práctico N°1 de la materia Análisis Numérico I de la FIUBA
 
 * **Camilo Fábregas** - [CFabregas](https://github.com/MiloGNR)
 * **Ignacio Latorre** - [ILatorre](https://github.com/ilatorre7)
-* **Juan Pablo Fresia** - [JPFresia](Link al Github)
+* **Juan Pablo Fresia** - [JPFresia](https://github.com/JuanPF56)
 * **Lucas Aisenberg** - [LAisenberg](https://github.com/lucaisen)
 * **Miguel Lederkremer** - [MLederkremer](Link al Github)
 
---
+++ TP1.m
@@ -0,0 +1,112 @@
+# Trabajo Práctico 1 - Grupo 17 - Modelación Númerica - 1er cuatrimestre 2021
+
+#Item a.
+datos = load('FlujoVehicular2019.dat'); # Lee el conjunto de datos del archivo.
+
+#------------------------------------------------------
+
+#FUNCIONES
+
+# Recibe una tabla de datos y va calculando el balance para cada hora.
+function balPorHora = balancePorHora(tabla)
+  
+  balPorHora = zeros(24,1);
+  
+  for i = 1:rows(tabla)
+    balPorHora = acumularBalancesPorHora(tabla,i,balPorHora);
+  endfor
+  
+endfunction
+
+# Recibe una tabla de datos y va calculando el balance para cada hora en una
+# estación específica.
+function balPorHora = balanceEstacionPorHora(tabla,estacion)
+  
+  balPorHora = zeros(24,1);
+  
+  for i = 1:rows(tabla)
+    if(tabla(i,5) == estacion)
+      balPorHora = acumularBalancesPorHora(tabla,i,balPorHora);
+    endif
+  endfor
+  
+endfunction
+
+# Acumula balances por hora en un vector y lo devuelve.
+function balPorHora = acumularBalancesPorHora(tabla,i,balancesPorHora)
+  
+  balPorHora = balancesPorHora;
+  
+  if(tabla(i,6) == 1) #Si es ingreso.
+    balPorHora(tabla(i,3)+1) += tabla(i,9); #Sumo cantidad de pasos a la hora.
+  else #Si es egreso.
+    balPorHora(tabla(i,3)+1) -= tabla(i,9); #Resto cantidad de pasos a la hora.
+  endif
+  
+endfunction
+
+# Busca y devuelve la estación de peaje con mayor cantidad de pasos totales.
+function estacion = mayorCantPasos(datos)
+  
+  pasosPorEstacion = zeros(8,1);
+  
+  for i = 1:rows(datos)
+    pasosPorEstacion(datos(i,5)) += datos(i,9); #Acumula pasos para c/estación.
+  endfor
+  
+  cantPasos = 0;
+  
+  for j = 1:8
+    if(pasosPorEstacion(j)>cantPasos) #Busca estacion con mayor cant. de pasos.
+      cantPasos = pasosPorEstacion(j);
+      estacion = j;
+    endif
+  endfor
+  
+  disp(pasosPorEstacion);
+  disp(estacion);
+  
+endfunction
+
+
+#------------------------------------------------------
+
+#Item b.
+figure(1);
+plot(0:23, balancePorHora(datos));
+title("Balances totales por hora.");
+
+#Item c.
+figure(2);
+estacion = mayorCantPasos(datos);
+titulo = strcat("Balance de estación con mayor cantidad de pasos (", int2str(estacion), ")");
+plot(0:23, balanceEstacionPorHora(datos,estacion));
+title(titulo);
+
+#Item d.
+
+#Item e.
+
+#Item f.
+
+#Item g.
+
+#Item h.
+
+#Item i.
+
+#------------------------------------------------------
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
